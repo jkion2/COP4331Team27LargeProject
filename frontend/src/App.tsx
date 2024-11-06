1b35aaa3896f1351a0555cff9e08f867a4554c4b
jkion2 <alvinalexabraham@gmail.com>
2024-11-06 05:15:51 +0000

    Phase 2 commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css';
-import LoginPage from './pages/LoginPage.tsx';
-function App()
-{
- return (
- <LoginPage />
- );
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; import './App.css';
+import LoginPage from './pages/LoginPage';
+import CardPage from './pages/CardPage';
+function App() {
+return (
+<Router >
+<Routes>
+<Route path="/" element={<LoginPage />} />
+<Route path="/cards" element={<CardPage />} />
+</Routes>
+</Router>
+);
 }
 export default App;
