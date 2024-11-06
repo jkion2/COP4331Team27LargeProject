8e0d17335c36741869b48d9e57d3331452471eb6
jkion2 <alvinalexabraham@gmail.com>
2024-11-06 05:54:38 +0000

    phase 4 commit

--- frontend/src/components/CardUI.tsx
+++ frontend/src/components/CardUI.tsx
@@ -1,23 +1,72 @@
 import React, { useState } from 'react';
 function CardUI()
 {
-
+let _ud : any = localStorage.getItem('user_data');
+let ud = JSON.parse( _ud );
+let userId : string = ud.id;
+let firstName : string = ud.firstName;
+let lastName : string = ud.lastName;
  const [message,setMessage] = useState('');
  const [searchResults,setResults] = useState('');
  const [cardList,setCardList] = useState('');
  const [search,setSearchValue] = React.useState('');
  const [card,setCardNameValue] = React.useState('');
 
- function addCard(e:any) : void
+ async function addCard(e:any) : Promise<void>
  {
  e.preventDefault();
- alert('addCard() ' + card);
+ let obj = {userId:userId,card:card};
+ let js = JSON.stringify(obj);
+ try
+ {
+ const response = await fetch('http://localhost:5000/api/addcard',
+ {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+ let txt = await response.text();
+ let res = JSON.parse(txt);
+ if( res.error.length > 0 )
+ {
+ setMessage( "API Error:" + res.error );
+ }
+ else
+ {
+ setMessage('Card has been added');
+ }
+ }
+ catch(error:any)
+ {
+ setMessage(error.toString());
+ }
  };
- function searchCard(e:any) : void
+ async function searchCard(e:any) : Promise<void>
  {
  e.preventDefault();
 
- alert('searchCard() ' + search);
+ let obj = {userId:userId,search:search};
+ let js = JSON.stringify(obj);
+ try
+ {
+ const response = await fetch('http://localhost:5000/api/searchcards',
+ {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+ let txt = await response.text();
+ let res = JSON.parse(txt);
+ let _results = res.results;
+ let resultText = '';
+ for( let i=0; i<_results.length; i++ )
+ {
+ resultText += _results[i];
+ if( i < _results.length - 1 )
+ {
+ resultText += ', ';
+ }
+ }
+ setResults('Card(s) have been retrieved');
+ setCardList(resultText);
+ }
+ catch(error:any)
+ {
+ alert(error.toString());
+ setResults(error.toString());
+ }
  };
  function handleSearchTextChange( e: any ) : void
  {
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -4,11 +4,39 @@ function Login()
     const [message,setMessage] = useState('');
     const [loginName,setLoginName] = React.useState('');
     const [loginPassword,setPassword] = React.useState('');
-function doLogin(event:any) : void
-{
-event.preventDefault();
-alert('doIt() ' + loginName + ' ' + loginPassword );
-};
+async function doLogin(event:any) : Promise<void>
+ {
+ event.preventDefault();
+ var obj = {login:loginName,password:loginPassword};
+ var js = JSON.stringify(obj);
+
+ try
+ {
+ const response = await fetch('http://localhost:5000/api/login',
+ {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+ var res = JSON.parse(await response.text());
+ 
+ if( res.id <= 0 )
+ {
+ setMessage('User/Password combination incorrect');
+ }
+ else
+ {
+ var user =
+{firstName:res.firstName,lastName:res.lastName,id:res.id}
+ localStorage.setItem('user_data', JSON.stringify(user));
+
+ setMessage('');
+ window.location.href = '/cards';
+ }
+ }
+ catch(error:any)
+ {
+ alert(error.toString());
+ return;
+ }
+ };
  function handleSetLoginName( e: any ) : void
  {
  setLoginName( e.target.value );
